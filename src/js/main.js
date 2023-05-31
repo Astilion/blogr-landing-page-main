4a22d5eaf50550bf586d0883162e3541c382c389
Mateusz <mkaminski.contact@gmail.com>
2023-05-31 18:16:35 +0200

    animation of nav items

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,35 @@
-const burgerIcon = document.querySelector(".burger-icon");
-const closeIcon = document.querySelector(".close-icon");
-const burgerBtn = document.querySelector(".burger-btn");
-const primaryItems = document.querySelectorAll(".nav__primary-item");
+document.addEventListener("DOMContentLoaded", () => {
+    const burgerIcon = document.querySelector(".burger-icon");
+    const closeIcon = document.querySelector(".close-icon");
+    const burgerBtn = document.querySelector(".burger-btn");
+    const primaryItems = document.querySelectorAll(".nav__primary-item");
+    const navMobile = document.querySelector(".nav__primary-items");
+  
+    const handleNav = () => {
+      closeIcon.classList.toggle("active");
+      burgerIcon.classList.toggle("active");
+      navMobile.classList.toggle("nav__primary-items--active");
+    };
+    function toggleSubMenu(item) {
+        const subMenu = item.nextElementSibling;
+    
+        primaryItems.forEach(primaryItem => {
+            if (primaryItem !== item) {
+                const otherSubMenu = primaryItem.nextElementSibling;
+                otherSubMenu.classList.remove("active");
+    
+            }
+        });
+        subMenu.classList.toggle("active");
+    }
+    
+    
+    primaryItems.forEach(item => {
+        item.addEventListener("click", () => {
+            toggleSubMenu(item);
+        });
+    });
+  
+    burgerBtn.addEventListener("click", handleNav);
 
-const handleNav = () => {
-	closeIcon.classList.toggle("active");
-	burgerIcon.classList.toggle("active");
-};
-
-function toggleSubMenu(item) {
-	const subMenu = item.nextElementSibling;
-
-	primaryItems.forEach(primaryItem => {
-		if (primaryItem !== item) {
-			const otherSubMenu = primaryItem.nextElementSibling;
-			otherSubMenu.classList.remove("active");
-
-		}
-	});
-	subMenu.classList.toggle("active");
-}
-
-burgerBtn.addEventListener("click", handleNav);
-
-primaryItems.forEach(item => {
-	item.addEventListener("click", () => {
-		toggleSubMenu(item);
-	});
-});
+  });
